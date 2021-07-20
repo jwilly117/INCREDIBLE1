7fc8a58e417a47a69988142020de58fb3063d68d
Jake Williams <jakewilliams117@gmail.com>
2021-07-20 00:03:23 -0400

    Psuedocoding

---
+++ app.js
@@ -0,0 +1,8 @@
+
+// I guess this is where we are going to make an express application
+// on 7/20/2021
+
+// Psuedocode for serve-index
+// 1. Make the application but instead of serving '/', serve the url that will
+//    be in the href for file library
+// 2. Then make a dummy uploader for the applicants (totally different page)
